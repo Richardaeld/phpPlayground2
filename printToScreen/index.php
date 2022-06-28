ef4d3b255ceaa14717bc9884320df6008f4bd34b
Richardaeld <Richardaeld@gmail.com>
2022-06-28 11:57:00 -0400

    first commit

---
+++ printToScreen/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<p>
+
+    <?php
+    $var = "something";
+    // Outputs one or more strings
+    echo "I am an echo", "\r\n", "with an additonal string";
+    echo "<br>";
+    // Outputs a string
+    print "I am a string";
+    print "<br>";
+    // Outputs human readable information about a variable
+    print_r($var);
+
+    ?>
+</p>
+
+</body>
+</html>
+
