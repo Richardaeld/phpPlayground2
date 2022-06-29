9719526492cf9e71a1c875d832d74e9e56689a39
Richardaeld <Richardaeld@gmail.com>
2022-06-28 20:24:28 -0400

    Print methods

--- printToScreen/index.php
+++ printToScreen/index.php
@@ -10,15 +10,50 @@
 <p>
 
     <?php
-    $var = "something";
+    // api call for varaible
+    $api_dice = 'https://dnd-rolling-chart-api.herokuapp.com/api/button/main/viewAll/children/3';
+    $json_api_dice = file_get_contents($api_dice);
+    $dice_data = json_decode($json_api_dice);
+    $var = $dice_data;
+    // $var = "something";
     // Outputs one or more strings
     echo "I am an echo", "\r\n", "with an additonal string";
+    echo "<br>";
+    echo "<br>";
+    // addition
+    echo "Im adding 7 to 7 -- ", 7+7;
+    echo "<br>";
     echo "<br>";
     // Outputs a string
     print "I am a string";
     print "<br>";
+    print "<br>";
     // Outputs human readable information about a variable
+    print_r("Whole Object:");
+    print_r("<br>");
     print_r($var);
+    print_r("<br>");
+    print_r("<br>");
+    print_r("Inside first object:");
+    print_r("<br>");
+    print_r($var->button);
+    print_r("<br>");
+    print_r("<br>");
+    print_r("Inside first object and objects array at 0:");
+    print_r("<br>");
+    print_r($var -> button[0]);
+    print_r("<br>");
+    print_r("<br>");
+    print_r("Inside first object and objects array at 0 within name:");
+    print_r("<br>");
+    print_r($var -> button[0] -> name);
+    print_r("<br>");
+    print_r("<br>");
+
+    // if else
+    // foreach
+    // syntax ->
+
 
     ?>
 </p>
